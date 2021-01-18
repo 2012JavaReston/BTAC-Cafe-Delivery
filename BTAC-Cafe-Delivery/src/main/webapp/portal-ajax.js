d7d938765eed6f6b654a4ba08f55266de57cef68
bjm630 <75638132+bjm630@users.noreply.github.com>
2021-01-18 14:27:24 -0600

    Update portal-ajax.js

--- BTAC-Cafe-Delivery/src/main/webapp/portal-ajax.js
+++ BTAC-Cafe-Delivery/src/main/webapp/portal-ajax.js
@@ -1,4 +1,24 @@
 window.onload = function(){
+    document.getElementById("logout").addEventListener('click', logout);
+    getMenu();
+    getOrders();
+}
+
+function logout(){
+    let xhttp = new XMLHttpRequest();
+
+    xhttp.onreadystatechange = function(){
+        if(xhttp.readyState == 4 && xhttp.status == 200){
+            alert("Logout successfull")
+        } else if(xhttp.readyState == 4 && xhttp.status != 200){
+            alert("Logout not successfull");
+        }
+    }
+
+    let logoutURL = "http://localhost:8080/BTAC-Cafe-Delivery/api/logout";
+    xhttp.open("GET", logoutURL);
+    xhttp.send();
+
 }
 
 function getMenu(){
@@ -13,7 +33,7 @@ function getMenu(){
         }
     }
 
-    let menuURL = "";
+    let menuURL = "http://localhost:8080/BTAC-Cafe-Delivery/api/items";
     xhttp.open("GET", menuURL);
     xhttp.send();
 }
@@ -47,10 +67,79 @@ function drawMenu(menuItems){
     }
 }
 
-function addToOrder(item){
+let order = new Array();
+order = {
+        items:{
+        "name": "coffee",
+        "cost": 2
+        },
+        items:{
+        "name": "banana",
+        "cost": 2
+        },
+        items:{
+        "name": "sandwich",
+        "cost": 2
+        }  
     
+};
+
+function addToOrder(item){
+    order[order.length] = item;
 }
 
 function removeFromOrder(item){
-    
+    const index = order.indexOf(item);
+    if (index > -1){order.splice(index, 1)}
+}
+
+function drawOrder(order){
+    let orderBody = document.getElementById("order-table-body");
+    //Clear current order
+    while(orderBody.firstChild){
+        orderBody.removeChild(orderBody.firstChild);
+    }
+    //Create table rows
+    for(let i=0; i<order.length; i++){
+        let tr = orderBody.insertRow(-1);
+        for(let j=0; j<order[0].length; j++){
+            let td = tr.insertCell(-1);
+            td.innerHTML = order[i][j];
+        }
+    }
+}
+
+
+function getOrders(){
+    let xhttp = new XMLHttpRequest();
+
+    xhttp.onreadystatechange = function(){
+        if(xhttp.readyState == 4 && xhttp.status == 200){
+            let orders = JSON.parse(xhttp.responseText);
+            drawOrders(orders);
+        } else if(xhttp.readyState == 4 && xhttp.status != 200){
+            alert("Could not load orders.");
+        }
+    }
+
+    let ordersURL = "http://localhost:8080/BTAC-Cafe-Delivery/api/orders";
+    xhttp.open("GET", ordersURL);
+    xhttp.send();
+}
+
+
+function drawOrders(orders){
+    let ordersBody = document.getElementById("orders-table-body");
+    //Clear current orders
+    while(ordersBody.firstChild){
+        ordersBody.removeChild(ordersBody.firstChild);
+    }
+    //Create table rows
+    for(let i=0; i<orders.length; i++){
+        let tr = ordersBody.insertRow(-1);
+        for(let j=0; j<orders[0].length; j++){
+            let td = tr.insertCell(-1);
+            td.innerHTML = orders[i][j];
+        }
+    }
 }
